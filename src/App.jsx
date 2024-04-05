f570af5579f297eeebad3e301aa336f1d01579df
Didier Alejandro Fajardo Annear <didier.fajardo@uptc.edu.co>
2024-04-04 22:48:32 -0500

    update

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ColorPicker from "ColorPicker/ColorPicker";
+import { useColors } from "ColorPicker/useColors";
+import ColorList from "ColorList/ColorList";
+import "./index.css";
+
+const App = () => {
+
+  const { 
+    color,
+    colorList,
+    handleChangeColor,
+    handleClickClearColors,
+    handleSubmitSaveColor,
+   } = useColors(); 
+
+  return(
+    <>
+  <h1 className="text-center bg-dark text-white p-2">Color Picker</h1>
+  <div className="container mt-4">
+    <div className="row">
+    <div className="col-12 col-md-4">
+    <ColorList 
+      colorList={colorList}
+      handleClickClearColors={handleClickClearColors}
+    />
+    </div>
+    <div className="col-12 col-md-8"> 
+    <ColorPicker 
+      color={color}
+      handleChangeColor={handleChangeColor}
+      handleSubmitSaveColor={handleSubmitSaveColor}
+    />
+    </div>
+    </div>
+  </div>
+
+    </>
+  );
+};
+ReactDOM.createRoot(document.getElementById("app")).render(<App />);
